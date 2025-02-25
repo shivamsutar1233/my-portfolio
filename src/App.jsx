183206ee81d481dbc351e430839c50fdde73b23c
Shivam Sutar <shivamsutar33@hotmail.com>
2025-02-25 13:23:15 +0530

    feat: adjust section padding for improved layout consistency

--- src/App.jsx
+++ src/App.jsx
@@ -22,32 +22,31 @@ function App() {
   return (
     <section className="app-container h-full min-h-screen bg-white text-black dark:bg-black dark:text-white relative">
       <Navbar setTheme={setTheme} theme={theme} />
-      {/* <NavigationLine /> */}
       {/* section for start */}
       <section id="Start" className=" relative">
         <NavigationLine title={"Start"} />
-        <section className="flex justify-center items-center h-screen">
+        <section className="flex justify-center pt-10 h-screen">
           <section className="text-3xl">Start</section>
         </section>
       </section>
       {/* section for about */}
       <section id="About" className=" relative">
         <NavigationLine title={"About"} />
-        <section className="flex justify-center items-center h-screen">
+        <section className="flex justify-center pt-10 h-screen">
           <section className="text-3xl">About</section>
         </section>
       </section>
       {/* section for projects */}
       <section id="Projects" className=" relative">
         <NavigationLine title={"Projects"} />
-        <section className="flex justify-center items-center h-screen">
+        <section className="flex justify-center pt-10 h-screen">
           <section className="text-3xl">Projects</section>
         </section>
       </section>
       {/* section for contact */}
       <section id="Contact" className=" relative">
         <NavigationLine title={"Contact"} />
-        <section className="flex justify-center items-center h-screen">
+        <section className="flex justify-center pt-10 h-screen">
           <section className="text-3xl">Contact</section>
         </section>
       </section>
